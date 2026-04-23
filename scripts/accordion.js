91e6b78a02637945a02368e2878237d3e9b7c894
Lucas Ângelo <lucasangeloss27@gmail.com>
2026-04-23 20:20:01 -0300

    Update accordion behavior to allow single active body
    Ensure only one accordion body is active at a time by removing 'active2' class from all bodies before adding it to the clicked one.

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -2,7 +2,12 @@ const accordions = document.querySelectorAll('.accordion');
 
 accordions.forEach(accordion => {
     accordion.addEventListener('click', () => {
+
+        document.querySelectorAll('.accordion_body').forEach(body => {
+            body.classList.remove('active2');
+        });
+
         const body = accordion.querySelector('.accordion_body');
-        body.classList.toggle('active2');
-    })
-})
+        body.classList.add('active2');
+    });
+});
